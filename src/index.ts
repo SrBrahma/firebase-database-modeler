c23fa601b777d41c1663bc20ab416f026be0b09f
Henrique Bruno <henrique.bruno.fa@gmail.com>
2020-09-25 20:11:39 -0300

    Fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // TODO: how to standardize optional model props?
 
-export { _, _$ } from './node';
-export { modelerSetDefaultDatabase as modelerSetDatabase, pathSegmentIsValid } from './functions';
+export { _, _$, _root } from './node';
+export { modelerSetDefaultDatabase, pathSegmentIsValid } from './functions';
 export { mEmptyObj, deepClone } from './utils';
 
 export { Reference } from '@firebase/database-types';
